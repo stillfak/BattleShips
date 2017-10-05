b0b8b9ead3a389862e5efc2cb585b747c855269f
vadim <vadim.gavrikov.1998@gmail.com>
2017-10-05 16:56:44 +0300

    добавлен метод checkship который проверяет попал ли пользователь в корабль

--- src/ru/gva_dve/battleShips/Ship.java
+++ src/ru/gva_dve/battleShips/Ship.java
@@ -1,33 +1,70 @@
 package ru.gva_dve.battleShips;
 
 
+import java.util.ArrayList;
+
+
 
 public class Ship {
     private int damageStatus;
     private int size;
-    private int[] position;
+    private ArrayList<String> position;
 
     public Ship( int size)  {
         this.size = size;
         this.damageStatus = 0;
     }
 
+    public Ship() {
+        this(3);
+    }
 
-    public void setPosition(int[] position) {
+    public void setPosition(ArrayList position) {
         this.position = position;
     }
 
-    public String shipsHits(){
-        int hitting;
-        while (this.damageStatus<this.size){
-            hitting = BattleShips.hits();
-            for (int i : position) {
-                if (i == hitting){
-                    damageStatus++;
-                }
-            }
-
-        }
+    @Override
+    public String toString() {
+        return "Ship{" +
+                "damageStatus=" + damageStatus +
+                ", size=" + size +
+                ", position=" + position +
+                '}';
     }
 
+    public int getDamageStatus() {
+        return damageStatus;
+    }
+    //    public String shipsHits(){
+//        int hitting;
+//        while (this.damageStatus<this.size){
+//            hitting = BattleShips.locationHits();
+//            for (String i : position) {
+//                if (i == hitting){
+//                    System.out.println("попал");
+//                    damageStatus++;
+//                    break;
+//                }else {
+//                    System.out.println("не попал");
+//                    break;
+//                }
+//            }
+//
+//        }
+//        return ;
+//    }
+
+   public String checkship(String shot){
+        String result = "мимо";
+        int index = position.indexOf(shot);
+        if (index != -1){
+            result = "ранил";
+            position.remove(index);
+            damageStatus++;
+        }
+        if (position.isEmpty()){
+            result = "убил";
+        }
+        return result;
+    }
 }
