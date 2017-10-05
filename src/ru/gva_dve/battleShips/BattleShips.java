fa3865b95e9cc3a6d47e7b23494b2ec32397081d
vadim <vadim.gavrikov.1998@gmail.com>
2017-10-05 16:54:28 +0300

    добавленно поле в виде arraylist`s, добавлен счетчик ходов.

--- src/ru/gva_dve/battleShips/BattleShips.java
+++ src/ru/gva_dve/battleShips/BattleShips.java
@@ -1,19 +1,35 @@
 package ru.gva_dve.battleShips;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BattleShips {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+        ArrayList<String> position = new ArrayList<>(7);
+        int travelCounter = 0;
+        position.add("1");
+        position.add("2");
+        position.add("3");
         Ship ship = new Ship();
-        ship.setPosition(new int[]{1,2,3});
-        ship.shipsHits();
+        ship.setPosition(position);
+        while (ship.getDamageStatus()<3) {
+            System.out.println(ship.checkship(locationHits()));
+            travelCounter++;
+        }
+        System.out.println("Кол-во ходов за игру - " + travelCounter);
+        //startgame(ship);
     }
 
-    public static int hits(){
+    public static String locationHits(){
         System.out.print("Введите координаты высстрела -");
-        return  sc.nextInt();
-    }
+        return  sc.next();
+   }
+
+//    public static void startgame(Ship ship) {
+//        System.out.println(ship.shipsHits());
+//    }
+
 
 }
