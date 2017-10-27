380660fb521a19b808994fbcc296ab50bba12577
vadim <vadim.gavrikov.1998@gmail.com>
2017-10-27 12:59:49 +0300

    Добавленны комментарии для классов BattleShips и Ship

--- src/ru/gva_dve/battleShips/BattleShips.java
+++ src/ru/gva_dve/battleShips/BattleShips.java
@@ -2,6 +2,11 @@ package ru.gva_dve.battleShips;
 
 import java.util.*;
 
+/**
+ * Содержит тестовый класс, содержит метод для ввода координат.
+ *
+ * @author Gavrikov V. && Dubinin V. 15IT18.
+ */
 
 public class BattleShips {
 
@@ -9,23 +14,36 @@ public class BattleShips {
     static Random rm = new Random();
 
     public static void main(String[] args) {
+
         ArrayList<String> position = new ArrayList<>();
+
         int travelCounter = 0;
         int onePointPosition = rm.nextInt(4);
+
         position.add(String.valueOf(onePointPosition));
         position.add(String.valueOf(onePointPosition + 1));
         position.add(String.valueOf(onePointPosition + 2));
+
         Ship ship = new Ship();
         ship.setPosition(position);
+
         while (!ship.getPosition().isEmpty()) {
+
             System.out.println(ship.checkship(locationHits()));
             travelCounter++;
+
         }
+
         System.out.println("Кол-во ходов за игру - " + travelCounter);
    }
 
+    /**
+     * Данный метод для ввода координат выстрела.
+     *
+     * @return координату выстрела строчного типа.
+     */
     public static String locationHits(){
-        System.out.print("Введите координаты высстрела -");
+        System.out.print("Введите координаты выстрела -");
         return  sc.next();
    }
     
