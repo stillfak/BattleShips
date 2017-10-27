3dcdb74580d22c67460582192783f03b27ee602f
vadim <vadim.gavrikov.1998@gmail.com>
2017-10-27 14:18:42 +0300

    создан дополнительный класс для ввода и проверки данных пользователя

---
+++ src/ru/gva_dve/battleShips/Input.java
@@ -0,0 +1,39 @@
+package ru.gva_dve.battleShips;
+
+import java.util.*;
+
+/**
+ * Вспомогательный класс для ввода данных.
+ *
+ * @author Gavrikov V && Dubinin V 15it18.
+ */
+public class Input {
+
+    static Scanner sc = new Scanner(System.in);
+
+
+    /**
+     * Данный метод для ввода координат выстрела.
+     *
+     * @return координату выстрела строчного типа.
+     */
+    public static String locationHits() {
+        System.out.println("Введите координаты выстрела");
+        System.out.println("от 0 до 9 ");
+        return checkhits(sc.nextInt());
+    }
+
+    /**
+     * Данный метод проверяет введеныые данные пользователем
+     *
+     * @return
+     */
+    private static String checkhits(int hits) {
+        if (hits > 9 || hits < 0) {
+            System.out.println("координаты данного ячейки поля не существует");
+            locationHits();
+        }
+        return String.valueOf(hits);
+
+    }
+}
